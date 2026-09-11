7d2b9e38e93c4aa317c62bde53d814b47d1b1336
Alvin Cabe <146591374+Alrwx@users.noreply.github.com>
2026-09-11 05:34:41 -0400

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # map of the freq of each letter, max size 26
+        counts = {}
+
+        #l and right pointers
+
+        max_count = 0
+        bigfrq = 0
+
+        l = 0
+        r = l
+
+        for r in range(len(s)):
+            counts[s[r]] = 1 + counts.get(s[r],0)
+            
+            while (r-l + 1) - max(counts.values()) > k:
+                #update the left pointer
+                counts[s[l]] -= 1
+                l += 1
+
+            max_count = max(max_count, r-l + 1)
+
+
+        return max_count
